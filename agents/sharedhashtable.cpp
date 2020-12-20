82a9a9d0eb4346bc41fe5393de9427083f39c94e
Dane Johnson <dane_johnson@ymail.com>
2020-12-20 15:06:29 -0600

    Fix invokations

--- agents/sharedhashtable.cpp
+++ agents/sharedhashtable.cpp
@@ -54,7 +54,9 @@ int SharedHashtable::sht(Board board, int depth, int alpha, int beta, bool maxim
     for (int move : moves) {
       value = std::min(value, sht(MakeMove(board, move), depth - 1, alpha, beta, true));
       beta = std::min(value, beta);
-      if (beta < )
+      if (beta <= alpha) { // Alpha cutoff
+	break;
+      }
     }
   }
   update(key, value);
@@ -65,12 +67,12 @@ int SharedHashtable::findMove(Board board, int depth) {
   std::vector<int> moves = GenerateMoves(board);
   // Send off other threads to fill out the table
   cilk_for (int i = 0; i < moves.size(); i++) {
-    sht(MakeMove(board, moves[i]), depth - 1, true);
+    sht(MakeMove(board, moves[i]), depth - 1, -MAX_VALUE, MAX_VALUE, true);
   }
   int best_value = -MAX_VALUE - 1;
   int best_move = -1;
   cilk_for (int i = 0; i < moves.size(); i++) {
-    int value = sht(MakeMove(board, moves[i]), depth - 1, true);
+    int value = sht(MakeMove(board, moves[i]), depth - 1, -MAX_VALUE, MAX_VALUE, true);
     if (value > best_value) {
       best_value = value;
       best_move = moves[i];

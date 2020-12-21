82cfa13493deb7b149ecd4ccfafaf1ceb78c8e17
Aaron Handleman <ahandleman@wustl.edu>
2020-12-21 13:36:47 -0600

    fixed data collection code

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Arg1 is a comma separated list of benchmarks, Arg2 is a comma separated list of classes, Arg3 is the runtimes to build on
-algorithms=${1:-SharedHashtable,ParallelPVS,ParallelAlphaBeta,Jamboree,SerialPVS}
+algorithms=${1:-ParallelPVS,ParallelAlphaBeta,Jamboree,SerialPVS,AlphaBeta}
 depths=${2:-12,13,14}
 work=100
 runs=50000
@@ -16,12 +16,12 @@ do
     	execution_output=$(taskset -c 0-$((c - 1)) ./othello -b $algorithm -w Random -d $depth)
         v=$(echo "$execution_output" | grep -E 'Black played by [^0-9]* [0-9]+' | grep -Eo '[0-9]+ microseconds' | grep -Eo '[0-9]+' )
 		echo Finished running $algorithm with $c cores >&2
-    	echo $algorithm , white , $v
+    	echo $algorithm , white , $c , $depth , $v
     	echo Running $algorithm with $c cores >&2
     	execution_output=$(taskset -c 0-$((c - 1)) ./othello -w $algorithm -b Random -d $depth)
         v=$(echo "$execution_output" | grep -E 'White played by [^0-9]* [0-9]+' | grep -Eo '[0-9]+ microseconds' | grep -Eo '[0-9]+' )
 		echo Finished running $algorithm with $c cores and depth $depth >&2
-    	echo $algorithm , black , $v
+    	echo $algorithm , black , $c , $depth , $v
     	if [[ "$algorithm" == "SerialPVS" ]]
     	then
     		break

71d565dc89195753c6ea8bb2bd28a46b389858f2
Aaron Handleman <ahandleman@wustl.edu>
2020-12-21 01:33:25 -0600

    ABDADA still needs to be done but the benchmarking works and jamboree now spawns based on number of workers

--- agents/jamboree.cpp
+++ agents/jamboree.cpp
@@ -1,6 +1,6 @@
 #include "jamboree.hpp"
-
 #include <cilk/cilk.h>
+#include <cilk/cilk_api.h>
 #include <cilk/reducer_max.h>
 
 #include <algorithm>
@@ -20,8 +20,43 @@ bool jambcompare(std::pair<int, Board> l, std::pair<int, Board> r) {
   return l.second.evaluate() > r.second.evaluate();
 }
 
-int jamboree(Board board, int depth, int alpha, int beta) {
+int serialJamboree(Board board, int depth, int a, int b) {
+  if (depth <= 0) return board.evaluate();  //if we've hit bottom depth
+  std::vector<std::pair<int, Board>> moves = GenerateMoveBoards(board);
+  //for (std::pair<int, Board> move_pair : moves) { std::cout << std::get<int>(move_pair) << ", ";} std::cout << std::endl;
+  if (moves.empty()) { //if we stall out then we check if we're a terminal node and otherwise stall.
+    int eval = board.isWinning();
+    if (eval > 0) return MAX_VALUE;
+    if (eval < 0) return -MAX_VALUE;
+    return 0;
+  }
+  std::pair<int, Board> primary_variation = moves[0];
+  int best_value = -MAX_VALUE - 1;
+  for (std::pair<int, Board> move_pair : moves) {
+    int score = -std::get<Board>(move_pair).evaluate();
+    if (score > best_value) {
+      best_value = score;
+      primary_variation = move_pair;
+    }
+  }
+  int score = -serialJamboree(std::get<Board>(primary_variation), depth - 1, -b, -a);
+  a = std::max(a, score);
+  for (std::pair<int, Board> move_pair : moves) {
+    if (a >= b) return score;
+    if (move_pair == primary_variation) continue;
+    score = std::max(score, -serialJamboree(std::get<Board>(move_pair), depth - 1, -a - 1, -a));
+    if ((score > a) && (score < b)) {
+      score = std::max(score, -serialJamboree(std::get<Board>(move_pair), depth - 1, -b, -score));
+    }
+    a = std::max(a, score);
+  }
+  return score;
+}
+
+
+int jamboree(Board board, int depth, int alpha, int beta, int serial_watcher) {
   if (depth <= 0) return board.evaluate();
+  if (serial_watcher <= 0) return serialJamboree(board, depth, alpha, beta);
   std::vector<std::pair<int, Board>> moves = GenerateMoveBoards(board);
   if (moves.empty()) {
     int eval = board.isWinning();
@@ -29,11 +64,22 @@ int jamboree(Board board, int depth, int alpha, int beta) {
     if (eval < 0) return -MAX_VALUE;
     return 0;
   }
-  std::sort(moves.begin(), moves.end(), jambcompare);
+  int size = moves.size();
+  int pv_index = 0;
   std::pair<int, Board> primary_variation = moves[0];
+  int best_value = -std::get<Board>(primary_variation).evaluate();
+  for (int i = 1; i < size; i++) {
+    int score = -std::get<Board>(moves[i]).evaluate();
+    if (score > best_value) {
+      best_value = score;
+      pv_index = i;
+    }
+  }
+  std::swap(moves[moves.size() - 1], moves[pv_index]);
+  moves.pop_back();
   // Evaluate the primary variation, younger brothers wait.
   int value;
-  value = -jamboree(primary_variation.second, depth -1, -beta, -alpha);
+  value = -jamboree(primary_variation.second, depth - 1, -beta, -alpha, serial_watcher - 1);
   if (value >= beta) { // Beta cutoff
     return value;
   }
@@ -41,10 +87,10 @@ int jamboree(Board board, int depth, int alpha, int beta) {
     alpha = value;
   }
   // Scout the remaining variations
-  int scouts[moves.size() - 1];
+  int scouts[moves.size()];
   int cutoff = -1;
-  cilk_for (int i = 1; i < moves.size(); i++) {
-    int s = -jamboree(moves[i].second, depth -1, -alpha -1, -alpha);
+  cilk_for (int i = 0; i < moves.size(); i++) {
+    int s = -jamboree(moves[i].second, depth - 1, -alpha - 1, -alpha, serial_watcher - 1);
     if (s >= beta) {
       // Beta cutoff
       cutoff = s;
@@ -64,29 +110,30 @@ int jamboree(Board board, int depth, int alpha, int beta) {
       value = scouts[i];
     }
     if (scouts[i] > alpha) {
-      value = std::max(value, -jamboree(move.second, depth -1, -beta, -alpha));
+      value = std::max(value, -jamboree(move.second, depth - 1, -beta, -alpha, serial_watcher - 1));
       alpha = std::max(alpha, value);
       if (alpha >= beta) { // Beta cutoff
-	return value;
+        return value;
       }
     }
   }
-  
+
   return value;
 }
 
 int Jamboree::findMove(Board board, int depth) {
   DEBUG(int failed_scouts = 0;)
-    DEBUG(int scouts = 0;)
-    DEBUG(int nodes = 0;)
-    std::vector<std::pair<int, Board>> moves = GenerateMoveBoards(board);
+  DEBUG(int scouts = 0;)
+  DEBUG(int nodes = 0;)
+  std::vector<std::pair<int, Board>> moves = GenerateMoveBoards(board);
+  int spawn_lvls = (32-__builtin_clz(__cilkrts_get_nworkers()))/2;
   cilk::reducer< cilk::op_max_index<int, int> > best_move; //soooo this is being misused a bit, but basically we do a max reducer in order to find the best move.  This is supposed to be for an index, but no reason it can't store the move instead
   cilk_for (int i = 0; i < moves.size(); i++) {
     std::pair<int, Board> move_pair = moves[i];
-    int score = -jamboree(std::get<Board>(move_pair), depth - 1, -MAX_VALUE - 1, MAX_VALUE + 1);
+    int score = -jamboree(std::get<Board>(move_pair), depth - 1, -MAX_VALUE - 1, MAX_VALUE + 1, spawn_lvls);
     best_move->calc_max(std::get<int>(move_pair), score);
   }
   DEBUG(printf("%d nodes searched.  Failed %d scouts out of %d scouts\n", nodes, failed_scouts, scouts);)
-    DEBUG(std::cout << std::flush;)
-    return best_move->get_index_reference();
+  DEBUG(std::cout << std::flush;)
+  return best_move->get_index_reference();
 }
---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+# Arg1 is a comma separated list of benchmarks, Arg2 is a comma separated list of classes, Arg3 is the runtimes to build on
+algorithms=${1:-SharedHashtable,ParallelPVS,ParallelAlphaBeta,Jamboree,SerialPVS}
+depths=${2:-12,13,14}
+work=100
+runs=50000
+for ((i = 0; i < 5; i++))
+do
+for ((depth = 8; depth <=12; depth += 1))
+do
+for algorithm in $(echo $algorithms | sed "s/,/ /g")
+do
+    for ((c = 1; c <= 32; c*=2)) ;
+    do
+    	echo Running $algorithm with $c cores and depth $depth >&2
+    	execution_output=$(taskset -c 0-$((c - 1)) ./othello -b $algorithm -w Random -d $depth)
+        v=$(echo "$execution_output" | grep -E 'Black played by [^0-9]* [0-9]+' | grep -Eo '[0-9]+ microseconds' | grep -Eo '[0-9]+' )
+		echo Finished running $algorithm with $c cores >&2
+    	echo $algorithm , white , $v
+    	echo Running $algorithm with $c cores >&2
+    	execution_output=$(taskset -c 0-$((c - 1)) ./othello -w $algorithm -b Random -d $depth)
+        v=$(echo "$execution_output" | grep -E 'White played by [^0-9]* [0-9]+' | grep -Eo '[0-9]+ microseconds' | grep -Eo '[0-9]+' )
+		echo Finished running $algorithm with $c cores and depth $depth >&2
+    	echo $algorithm , black , $v
+    	if [[ "$algorithm" == "SerialPVS" ]]
+    	then
+    		break
+	    fi
+	    if [[ "$algorithm" == "AlphaBeta" ]]
+    	then
+    		break
+	    fi
+	done
+done
+done
+done
